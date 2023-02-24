d7406052b2c22554e5049c79223133b1c7ab6adc
delmaSong <sdy2856@gmail.com>
2023-02-24 20:00:43 +0900

    chore: 계층구조 수정

---
+++ Sources/Resource/Resource+Color.swift
@@ -0,0 +1,27 @@
+//
+//  Resource+Color.swift
+//  
+//
+//  Created by Delma Song on 2023/02/22.
+//
+
+import UIKit
+
+public extension Resource {
+    enum Color {
+        // MARK: - GrayScale
+        public static let white = UIColor(named: "white", in: .module, compatibleWith: nil)!
+        public static let gray100 = UIColor(named: "gray100", in: .module, compatibleWith: nil)!
+        public static let gray400 = UIColor(named: "gray400", in: .module, compatibleWith: nil)!
+        public static let black = UIColor(named: "black", in: .module, compatibleWith: nil)!
+        
+        // MARK: - Red
+        public static let red100 = UIColor(named: "red100", in: .module, compatibleWith: nil)!
+        
+        // MARK: - Blue
+        public static let blue100 = UIColor(named: "blue100", in: .module, compatibleWith: nil)!
+        
+        // MARK: - Semantic
+        public static let borderLine = gray100
+    }
+}
