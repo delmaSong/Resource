c5a3f1e19ccd73420ffb5a5449f2873940675a0b
delmaSong <sdy2856@gmail.com>
2023-02-27 17:00:38 +0900

    사이즈 수정

--- Sources/Resource/Resource+Font.swift
+++ Sources/Resource/Resource+Font.swift
@@ -10,18 +10,19 @@ import UIKit
 public extension Resource {
     enum Font {
         // MARK: - Header
-        public static let header1 = UIFont.systemFont(ofSize: 32, weight: .bold)
-        public static let header2 = UIFont.systemFont(ofSize: 30, weight: .semibold)
+        public static let header1 = UIFont.systemFont(ofSize: 26, weight: .bold)
+        public static let header2 = UIFont.systemFont(ofSize: 18, weight: .semibold)
         
         // MARK: - Title
         public static let title1 = UIFont.systemFont(ofSize: 26, weight: .bold)
         public static let title2 = UIFont.systemFont(ofSize: 24, weight: .medium)
-        public static let title3 = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        public static let title3 = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        public static let title4 = UIFont.systemFont(ofSize: 10, weight: .regular)
                 
         // MARK: - Subtitle
-        public static let subtitle1 = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        public static let subtitle1 = UIFont.systemFont(ofSize: 12, weight: .medium)
         
         // MARK: - Body
-        public static let body1 = UIFont.systemFont(ofSize: 14, weight: .regular)
+        public static let body1 = UIFont.systemFont(ofSize: 10, weight: .regular)
     }
 }
